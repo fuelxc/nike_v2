f6080d6b032dddcc0b070a6c94cbbf11574c6e52
Eric Harrison <eharrison@smashtankapps.com>
2013-01-27 21:22:35 -0700

    make each of the summaries instance vars

--- lib/nike_v2/summary.rb
+++ lib/nike_v2/summary.rb
@@ -8,7 +8,16 @@ module NikeV2
     def initialize(attributes = {})
       raise "#{self.class} requires a person." unless attributes.keys.include?(:person)
       set_attributes(attributes)
-      super(fetch_data)
+      super(initialize_data)
+    end
+
+    private
+    def initialize_data
+      initialization_data = {}
+      fetch_data['summaries'].each do |data|
+        initialization_data[data['experienceType'].downcase] = data['records']
+      end
+      initialization_data
     end
   end
 end
