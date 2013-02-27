6fdb66fd85e022fbab3b18ced70c9853dfa9202a
Parth Barot <parth.barot@botreeconsulting.com>
2013-02-27 14:54:02 +0530

    issue #7,
    1. Added new method 'fetch_all' to fetch all activities.
    2. Refactored the code, added new private method 'fetch_and_build_activities'

--- lib/nike_v2/activities.rb
+++ lib/nike_v2/activities.rb
@@ -28,24 +28,43 @@ module NikeV2
 
     def fetch_more
       unless self.paging['next'].blank?
-        url, query = self.paging['next'].match(/^(.*?)\?(.*)$/)[1,2]
-        query = query.split(/&/).inject({}){|h,item| k, v = item.split(/\=/); h[k] = v;h}
-        activities = fetch_data(url, {query: query})
-        build_activities(activities.delete('data'))
-        
-        # 1. Changing [] with () for syntax error in '.delete' method call
-        
-        # 2. Changing "self.paging=" to "@paging", because no accessor method present. 
-        # Instead, We can also change this in resource.rb, to define new setter method for each attribute.
-        @paging = activities.delete('paging') 
+        fetch_and_build_activities 
       end
     end
 
+    # Added by: Parth Barot, 27 Feb,2013.
+    # Issue 7
+    #
+    def fetch_all
+      until self.paging['next'].blank? do
+        fetch_and_build_activities 
+      end
+    end
+
+    #######################
     private
+    #######################
+
     def build_activities(data)
       data.each do |activity|
         self << NikeV2::Activity.new({:person => self}.merge(activity))
       end
+    end
+
+    # Refactored by: Parth Barot, 27 Feb,2013.
+    # Needs to be extracted as a private method, to avoid code duplication for new method 'fetch_all'
+    #
+    def fetch_and_build_activities
+      url, query = self.paging['next'].match(/^(.*?)\?(.*)$/)[1,2]
+      query = query.split(/&/).inject({}){|h,item| k, v = item.split(/\=/); h[k] = v;h}
+      activities = fetch_data(url, {query: query})
+      build_activities(activities.delete('data'))
+      
+      # 1. Changing [] with () for syntax error in '.delete' method call
+      
+      # 2. Changing "self.paging=" to "@paging", because no accessor method present. 
+      # Instead, We can also change this in resource.rb, to define new setter method for each attribute.
+      @paging = activities.delete('paging')
     end
   end
 end
